ae036353637dd43b0ffb75222f12c0a374b5d5f9
KingCosmic <KingCosmicDev@gmail.com>
2021-01-09 01:28:01 -0600

    fixed jwt validator to work with mongodb driver

--- src/validators/jwt.ts
+++ src/validators/jwt.ts
@@ -1,5 +1,6 @@
 import { UserType } from '../types/user'
 import Db from '../db'
+import { ObjectId } from 'mongodb'
 
 // TODO: add in more checks (maybe a token, or something similar)
 // rn all we check is if there's a user with that id, people could forge jwt's
@@ -9,7 +10,7 @@ export default async (decoded, request, h) => {
 
   try {
     const users = await Db.Users.find({
-      _id: decoded.id,
+      _id: ObjectId.createFromHexString(decoded),
     })
     .limit(1).toArray()
 
