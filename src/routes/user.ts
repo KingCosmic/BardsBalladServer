2dba9f3e930ba8f4791721f02eaa36c5a4a34aa7
KingCosmic <KingCosmicDev@gmail.com>
2021-07-27 21:23:21 -0500

    fixed issues with logging in and signing up

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -54,7 +54,7 @@ routes.push({
     auth: false,
     validate: {
       payload: Joi.object({
-        username: Joi.string().min(1).max(20).required(),
+        email: Joi.string().min(1).required(),
         password: Joi.string().min(7).required()
       })
     }
@@ -96,7 +96,7 @@ routes.push({
     auth: false,
     validate: {
       payload: Joi.object({
-        email: Joi.string().min(1).max(20).required(),
+        email: Joi.string().min(1).required(),
         password: Joi.string().min(7).required()
       })
     }
